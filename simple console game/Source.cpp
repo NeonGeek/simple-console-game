84ea07af959c5258e9f9f8129e363ed5df2d16a7
NeonGeek <jambikez@yandex.ru>
2016-06-09 18:21:39 +0500

    initializating project

---
+++ simple console game/Source.cpp
@@ -0,0 +1,15 @@
+/* LAB#7: SIMPLE CONSOLE GAME
+BY CIS-102 STUDENT
+ARTYOM TIURIN
+2015-2016*/
+
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+
+}
